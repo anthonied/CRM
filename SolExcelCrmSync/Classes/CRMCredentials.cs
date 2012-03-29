9fbe09aae4a6250f8894016ce51663f75e705782
Anthonie de Klerk <anthonied@solutionserver.co.za>
2012-03-29 09:58:56 +0200

    Working Account Add

--- SolExcelCrmSync/Classes/CRMCredentials.cs
+++ SolExcelCrmSync/Classes/CRMCredentials.cs
@@ -10,14 +10,20 @@ namespace SolExcelCrmSync.Classes
     public class CRMCredentials
     {
         public ClientCredentials Credentials { get; set; }
+        public ClientCredentials DeviceCredentials { get; set; }
         public Uri OrganizationUri { get; set; }
 
         public CRMCredentials()
         {
             Credentials = new ClientCredentials();
             Credentials.UserName.UserName = ConfigurationManager.AppSettings["CRMServiceUserName"];
-            Credentials.UserName.Password = ConfigurationManager.AppSettings["CRMServicePassword"];
+            Credentials.UserName.Password = ConfigurationManager.AppSettings["CRMPassword"];
             OrganizationUri = new Uri(ConfigurationManager.AppSettings["OrganizationEndPointURI"]);
+
+            DeviceCredentials = new ClientCredentials();
+            DeviceCredentials.UserName.UserName = ConfigurationManager.AppSettings["CRMServiceUserName"];
+            DeviceCredentials.UserName.Password = ConfigurationManager.AppSettings["CRMServicePassword"];
+
         }
 
         
